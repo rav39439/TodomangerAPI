61f29aaa3fdeb1bef1af306f1ad48da26e74c6a6
rav39439 <ravish.kumar@ispf.ngo>
2025-05-14 18:32:45 +0530

    updated the frontend link

--- index.js
+++ index.js
@@ -18,23 +18,23 @@ const conn = mongoose.createConnection(process.env.MONGO_KEY,{ useNewUrlParser: 
 
 
 
-// app.use(cors(), function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin","https://taskmanagerforusers.netlify.app"); // update to match the domain you will make the request from
-//     res.header(
-//       "Access-Control-Allow-Headers",
-//       "Origin, X-Requested-With, Content-Type, Accept"
-//     );
-//     next();
-//   });
+app.use(cors(), function(req, res, next) {
+    res.header("Access-Control-Allow-Origin","https://todomanagerfrontend.netlify.app"); // update to match the domain you will make the request from
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000/');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000/');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept'
+//   );
+//   next();
+// });
   app.use(express.json());
 
   app.use("/api/users", userRoute);
